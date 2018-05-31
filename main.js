969b51434eaae5c33438658720d7df54dbc15a8a
Antonio Raimundo <antonioraimundo007@gmail.com>
2018-05-30 23:33:42 -0300

    update index

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express.js')();
 
-
+//Create Server end set port
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server running at port ' + app.get('port'));
 });
